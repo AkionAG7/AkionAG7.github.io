fec196b6be8d22031ebadcd3db2a5bb3f755175e
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-04-22 00:29:05 -0600

    imagenes perdidas

--- src/features/Proyects/Assets/ProyectList.ts
+++ src/features/Proyects/Assets/ProyectList.ts
@@ -4,7 +4,7 @@ const ProyectList = [
     Description:
       "Proyecto de gestion de cursos universitarios para una universidad fictisia llamada Three Duckling University, se podra crear, editar y eliminar cursos, ademas podemos ver los cursos de manera paginada y filtrar por nombre de curso.",
     Imagen:
-      "https://media.discordapp.net/attachments/1152115445080526888/1363708738791411782/University_Three_Duckling.png?ex=6807043b&is=6805b2bb&hm=9104244dfc7512340318279976a2e6186cf7d6ae5039f73f9f969fdc0368b022&=&format=webp&quality=lossless&width=1082&height=744",
+      "https://media.discordapp.net/attachments/869734579722928178/1364125533083926589/University_Three_Duckling.png?ex=68088867&is=680736e7&hm=bcf37e5d0efa2a6c8024c3568675f8d65bb32a3f5be8cb63dc5de9f35e98c380&=&format=webp&quality=lossless&width=1082&height=744",
     Deploy: "https://courses-manage.vercel.app/",
     Tecnologies: ["HTML", "JavaScript", "CSS"],
   },
@@ -14,7 +14,7 @@ const ProyectList = [
     Description:
       "El proyecto es un sistemas de gestion de activos de la biblioteca pública municipal de Nicoya en la cual permite añadir, modificar, eliminar activos de la bilioteca, gestion de usuarios, gestion de prestamos y devoluciones de libros, y gestionar solicitudes de voluntariado y salas, ademas del sistema posee una imagen informativa la cual exhibe a los usuarios los servicios con los que cuenta la biblioteca.",
     Imagen:
-      "https://media.discordapp.net/attachments/1152115445080526888/1363708738355073104/Hogar_de_libros.png?ex=6807043b&is=6805b2bb&hm=cd411147d59aa9bb8546154d8dec4cbe8a9e60574188152a57ff74a43c67655c&=&format=webp&quality=lossless&width=1620&height=744",
+      "https://media.discordapp.net/attachments/869734579722928178/1364125533608083528/Hogar_de_libros.png?ex=68088867&is=680736e7&hm=f8a05b351b3ebf2a88f629630c3fc58ee288b8331163cffbc05ec6a0ffdae33b&=&format=webp&quality=lossless&width=1620&height=744",
     Deploy: "https://hogar-de-libros-front-zer0.vercel.app/",
     Tecnologies: ["React", "TailWind", "NestJS", "MySQL", "Docker", "FlowBite"],
   },
